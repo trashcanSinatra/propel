cceb56dc7f013a8657ba8f62ed181a5e9fb06d69
Aaron Vogt <avogt@spokencode.net>
2015-11-17 23:21:31 -0500

    Updated initialize method to optionally set API Key

--- lib/propel/gcm.rb
+++ lib/propel/gcm.rb
@@ -1,7 +1,12 @@
 class GCM < Push
 
-   def initialize
-      puts 'made it from GCM'
+   attr_accessor :appKey
+
+   def initialize(appKey = false)
+      unless appKey
+         @appKey = appKey
+      end
+      puts 'made it from GCM' + "\n\n#{appKey}"
    end
-   
+
 end
