cea79fd946977b31d5c233333c8278151ca8457f
Aaron Vogt <avogt@spokencode.net>
2015-11-17 22:00:07 -0500

    Added Notification Stream Method for creating subclass instances

--- lib/propel/push.rb
+++ lib/propel/push.rb
@@ -1,1 +1,12 @@
-puts "Aaron"
+class Push
+
+   @@types = { 'google' => 'GCM', 'apple' => 'APNS' }
+
+   def self.NotificationStream(type)
+      return false unless @@types.has_key?(type)
+      cls = Object.const_get(@@types[type])
+      pushMananger = cls.new
+      return pushMananger
+   end
+
+end
