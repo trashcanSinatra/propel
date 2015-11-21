092c35d9cc09066ad4fb8b900108e8b2a7db09ab
Aaron Vogt <atvogt@localhost.localdomain>
2015-11-20 21:04:41 -0500

    push class structure
    Added singleton class structure to Push class static methods

--- lib/propel/push.rb
+++ lib/propel/push.rb
@@ -1,12 +1,24 @@
 class Push
 
    @@types = {'google' => 'GCM', 'apple' => 'APNS'}
+   @@tokens = []
+   @@messages = []
 
-   def self.OpenStream(type, *args)
-      return false unless @@types.has_key?(type)
-      cls = Object.const_get(@@types[type])
-      pushMananger = cls.new(*args)
-      return pushMananger
-   end
+   class << self
 
-end
+      def OpenStream(type, *args)
+         return false unless @@types.has_key?(type)
+         # Technique for getting Class from string
+         cls = Object.const_get(@@types[type])
+         pushMananger = cls.new(*args)
+         return pushMananger
+      end
+
+      def push_manager(*args)
+         return false if self == Push
+         return self.new(*args)
+      end
+   end # Class Self
+
+
+end  # Class Push
