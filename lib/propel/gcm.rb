9f551a55dbd03856e830ef444b240132f1cd41b9
Aaron Vogt <atvogt@localhost.localdomain>
2015-11-22 17:44:42 -0500

    Attribute Accessors GCM
    Added postHeader, appKey instance variables.

--- lib/propel/gcm.rb
+++ lib/propel/gcm.rb
@@ -1,16 +1,48 @@
+require 'http'
+
 class GCM < Push
 
    @tokens = []
    @messages = []
-
-   
-   attr_accessor :appKey
+   attr_reader :appKey
 
    def initialize(appKey = false)
-      if appKey
-         @appKey = appKey
-      end
-      puts 'made it from GCM' + "\n\n#{appKey}"
+      super('https://android.googleapis.com/gcm/send')
+      @appKey = appKey || ""
+      @postHeaders = {:'Content-Type' => 'application/json',
+                      :Authorization => "key=#{@appKey}"}
    end
+
+
+   def postHeaders
+      return @postHeaders
+   end
+
+
+   def addHeader(key, value)
+      @postHeaders[key] = value
+   end
+
+
+   def delHeader(header)
+      if @postHeaders.has_key?(header)
+         @postHeaders.delete(header)
+      else
+         puts "The key \"#{header}\" doesn't exist."
+      end
+   end
+
+
+   def appKey=(key)
+      @appKey = key
+      addHeader(:Authorization, "key=#{key}")
+   end
+
+
+   def push(tkns, msgs)
+
+   end
+
+
 
 end
